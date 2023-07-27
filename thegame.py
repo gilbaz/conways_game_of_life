9554444056a1902d086da6dacf76f868547fdefa
gilbaz <gil.elbaz13@gmail.com>
2023-07-27 16:29:16 +0300

    * changing example

--- thegame.py
+++ thegame.py
@@ -139,7 +139,7 @@ if __name__ == "__main__":
 
     start_world = State(world)
 
-    # start_world.override_local_state((30,30),KnownCreatureFactory.create_acorn())
+    start_world.override_local_state((30,30),KnownCreatureFactory.create_acorn())
 
     start_world.override_local_state((8,9),KnownCreatureFactory.create_glider())
     start_world.override_local_state((40,35),KnownCreatureFactory.create_tub())
